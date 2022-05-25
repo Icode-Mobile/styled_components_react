b2ff733b16fa62deedf1f35e8015200b15f7adff
Caio18-cosenza <caiiocosenza2020@gmail.com>
2022-05-25 14:00:28 -0300

    Styled React Done

---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import { Container, Title, SubTitle } from './style';
+
+export default function Home() {
+  return (
+    <Container>
+      <Title> Icode Mobile </Title>
+      <SubTitle> Aprendendo Styled Components </SubTitle>
+    </Container>
+  );
+}
---
+++ src/Pages/Home/style.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  ///Aqui eu escrevo o meu css
+  background-color: #222;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.h1`
+  color: white;
+  font-size: 50px;
+  font-weight: 700;
+  font-family: 'Roboto', sans-serif;
+`;
+
+export const SubTitle = styled.h3`
+  color: #ccc;
+  font-weight: 300;
+  font-family: 'Roboto', sans-serif;
+`;
